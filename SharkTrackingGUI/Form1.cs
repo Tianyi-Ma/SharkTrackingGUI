637b038c6dedda0ed6cd415b2bc582ea25f3b2b7
Tianyi <Tianyi@TIANYI-PC>
2017-06-29 14:01:42 -0700

    Finished Plotting

---
+++ SharkTrackingGUI/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace SharkTrackingGUI
+{
+    public partial class Form1 : Form
+    {
+        public FormModel Model = new FormModel();
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            TrackPlot.Series.Add("Shark").YValueMembers = "Shark";
+            TrackPlot.Series["Shark"].ChartType = SeriesChartType.FastPoint;
+            TrackPlot.Series["Shark"].XValueType = ChartValueType.Double;
+            TrackPlot.Series["Shark"].YValueType = ChartValueType.Double;
+
+            foreach (var element in Model.SharkXY)
+            {
+                TrackPlot.Series["Shark"].Points.AddXY(element.X, element.Y);
+            }
+
+            TrackPlot.Series.Add("AUV").YValueMembers = "AUV";
+            TrackPlot.Series["AUV"].ChartType = SeriesChartType.FastPoint;
+            TrackPlot.Series["AUV"].XValueType = ChartValueType.Double;
+            TrackPlot.Series["AUV"].YValueType = ChartValueType.Double;
+            TrackPlot.ChartAreas["ChartArea1"].AxisX.LabelStyle.Format = "#.##";
+
+            foreach (var element in Model.AUVXY)
+            {
+                TrackPlot.Series["AUV"].Points.AddXY(element.X, element.Y);
+            }
+            //            TrackPlot.Series["Shark"].Points.DataBind(SharkModel.SharkXY);
+
+        }
+
+        private void TrackPlot_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ SharkTrackingGUI/FormModel.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharkTrackingGUI
+{
+    /// <summary>
+    /// This class extracts data from AUV and creates a model for the GUI
+    /// </summary>
+    public class FormModel
+    {
+        public struct XY
+        {
+            public double X { get; set; }
+            public double Y { get; set; }
+            public XY(double x, double y)
+            {
+                X = x;
+                Y = y;
+            }
+        }
+
+        public List<XY> SharkXY = new List<XY>();
+        public List<XY> AUVXY = new List<XY>();
+
+        public FormModel()
+        { 
+            SharkXY.Add(new XY(1.0, 2.9));
+            SharkXY.Add(new XY(2.0, 4.0));
+            SharkXY.Add(new XY(2.5, 7.0));
+            AUVXY.Add(new XY(5.2, 3.5));
+            AUVXY.Add(new XY(5.6, 3.2));
+            AUVXY.Add(new XY(2.3, 9.0));
+
+        }
+
+        //Setting the maximum number of points to be plot to be 100
+        public void UpdateSharkXY()
+        {
+
+        }
+
+        public void UpdateAUVXY()
+        {
+
+        }
+
+        public void GetSharkXY()
+        {
+
+        }
+
+        public void GetAUVXY()
+        {
+
+        }
+        public class TupleList<T1, T2> : List<Tuple<T1, T2>>
+        {
+            public void Add(T1 item, T2 item2)
+            {
+                Add(new Tuple<T1, T2>(item, item2));
+            }
+        }
+
+    }
+
+
+}
